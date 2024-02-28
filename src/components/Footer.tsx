7cf9dee525d16bf83396c1f9298d9022c327cc10
Niklas Lühr <niklas.luehr1@gmail.com>
2024-02-28 22:46:09 +0700

    add space

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <footer className="absolute bottom-2 flex w-full justify-center text-center">
       <span className="text-xs opacity-60">
         Copyright © Niklas Lühr
-        {new Date().getFullYear()}
+        {" " + new Date().getFullYear()}
       </span>
     </footer>
   );
