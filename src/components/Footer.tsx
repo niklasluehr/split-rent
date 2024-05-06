6113609f7922545e9ead461050b95745ecb4383f
Niklas Lühr <niklas.luehr1@gmail.com>
2024-05-06 13:34:57 +0800

    add link to portfolio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,15 @@ export const Footer = () => {
   return (
     <footer className="absolute bottom-2 flex w-full justify-center text-center">
       <span className="text-xs opacity-60">
-        Copyright © Niklas Lühr
+        Copyright ©{" "}
+        <a
+          className="underline"
+          href="https://niklasluehr.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Niklas Lühr
+        </a>
         {" " + new Date().getFullYear()}
       </span>
     </footer>
